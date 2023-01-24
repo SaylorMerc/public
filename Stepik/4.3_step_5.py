209803b79588d3a0c622598c1bd878f399e4da8b
SaylorMerc <Kirukato.EVE@yandex.ru>
2023-01-24 17:52:58 +0300

    create files

---
+++ Stepik/4.3_step_5.py
@@ -0,0 +1,9 @@
+# Даны три различных целых числа. Напишите программу, которая находит среднее по величине число.
+
+a, b, c = int(input()), int(input()), int(input())
+if (b < a < c) or (b > a > c):
+    print(a)
+elif (a < b < c) or (a > b > c):
+    print(b)
+elif (b < c < a) or (b > c > a):
+    print(c)
